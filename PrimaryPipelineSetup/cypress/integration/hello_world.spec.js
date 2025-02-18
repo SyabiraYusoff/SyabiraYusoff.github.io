5131188ff46ebb9c295ac8fb827d9bc63be59a14
SyabiraYusoff <syabira.mdyusoff@gmail.com>
2025-02-18 08:43:37 +0000

    add gitignore for some files

--- PrimaryPipelineSetup/cypress/integration/hello_world.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Application Load Test', () => {
-    it('should load the application correctly', () => {
-        cy.visit('http://localhost:3000');
-        cy.contains('Welcome'); // Adjust the text based on your application
-    });
-});
